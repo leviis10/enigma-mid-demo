5a5b7f7ba3bc63e6b5cc314b20d2652297d05928
Levi <alevindrajaya@gmail.com>
2024-07-19 09:01:15 +0700

    initial commit

---
+++ src/main/java/enigma/midtrans/MidtransApplication.java
@@ -0,0 +1,13 @@
+package enigma.midtrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtransApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enigma/midtrans/MidtransApplicationTests.java
@@ -0,0 +1,13 @@
+package enigma.midtrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
